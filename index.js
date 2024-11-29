1dc9999951b838366e21f502292403e9239e2a10
Pablo Bru <pablobru08@gmail.com>
2024-11-29 20:04:17 -0300

    ejercicio finalizado

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const etiquetaUl = document.querySelector(".lista");
+  const etiquetasLi = etiquetaUl.querySelectorAll("li");
+  etiquetasLi.forEach((rem) => rem.remove());
+  //console.log(etiquetasLi);
+  cosasQueAprendimos.forEach((i) => {
+    const nuevoLi = document.createElement("li");
+    nuevoLi.innerHTML = i.tema;
+    if (i.class !== "") {
+      nuevoLi.classList.add(i.class);
+    }
+    //console.log(nuevoLi);
+    document.querySelector(".lista").appendChild(nuevoLi);
+  });
+}
 
 main();
